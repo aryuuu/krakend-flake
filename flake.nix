59992d78c5a7c6b714bc45affa413d72f373eb7e
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2025-03-12 18:29:59 +0700

    feat: init catalog

--- flake.nix
+++ flake.nix
@@ -13,40 +13,92 @@
           system = system;
         }
       );
+      
+      # Helper function to determine the appropriate architecture mapping
+      getSystemArch = system:
+        let
+          parts = builtins.split "-" system;
+          arch = builtins.elemAt parts 0;
+          platform = builtins.elemAt parts 2;
+        in
+          if arch == "x86_64" then "amd64"
+          else if arch == "aarch64" then "arm64"
+          else null;
+          
+      # Helper to get platform mapping
+      getSystemPlatform = system:
+        let
+          parts = builtins.split "-" system;
+          platform = builtins.elemAt parts 2;
+        in
+          if platform == "linux" then "generic-linux"
+          else if platform == "darwin" then "darwin"
+          else null;
+      
+      # Read the catalog file
+      catalogFile = ./catalog.json;
+      catalog = if builtins.pathExists catalogFile 
+                then builtins.fromJSON (builtins.readFile catalogFile)
+                else {};
+      
+      # Create a package for a specific version and system
+      makeKrakendPackage = system: version:
+        let
+          pkgs = pkgsFor.${system};
+          systemArch = getSystemArch system;
+          systemPlatform = getSystemPlatform system;
+          
+          # Find the appropriate build for this system
+          buildInfo = lib.findFirst 
+            (build: build.arch == systemArch && build.platform == systemPlatform) 
+            null 
+            (catalog.versions.${version}.builds or []);
+        in
+          if buildInfo == null then null else
+          pkgs.stdenv.mkDerivation {
+            pname = "krakend";
+            inherit version;
+            
+            src = pkgs.fetchurl {
+              url = buildInfo.url;
+              sha256 = buildInfo.sha256;
+            };
+            
+            sourceRoot = ".";
+            
+            nativeBuildInputs = with pkgs; [
+              autoPatchelfHook
+            ];
+            
+            buildInputs = with pkgs; [
+              stdenv.cc.cc.lib
+            ];
+            
+            installPhase = ''
+              mkdir -p $out/bin
+              install -m755 -D usr/bin/krakend $out/bin/krakend
+            '';
+          };
     in
     {
-      devShells = eachSystem (system: {
-        default = pkgsFor.${system}.mkShell {
-          packages = [
-          ];
-        };
-      });
-      packages = eachSystem (system: {
-        default = pkgsFor.${system}.stdenv.mkDerivation rec {
-          pname = "krakend";
-          version = "2.7.0";
-          arch = "x86_64-linux";
+      packages = eachSystem (system:
+        let
+          # Default to latest version in catalog
+          latestVersion = lib.head (lib.sort lib.versionAtLeast (builtins.attrNames (catalog.versions or {})));
           
-          src = pkgsFor.${system}.fetchurl {
-            url = "https://github.com/krakend/krakend-ce/releases/download/v${version}/krakend_${version}_amd64_generic-linux.tar.gz";
-            sha256 = "sha256-hMsiK9IyL1mMZg83Dp1sdY+oYFQ+eIkcnc2lzdEkFNQ=";
-          };
-          
-          sourceRoot = ".";
-          
-          nativeBuildInputs = with pkgsFor.${system}; [
-            autoPatchelfHook
-          ];
-          
-          buildInputs = with pkgsFor.${system}; [
-            stdenv.cc.cc.lib
-          ];
-          
-          installPhase = ''
-            mkdir -p $out/bin
-            install -m755 -D usr/bin/krakend $out/bin/krakend
-          '';
-        };
-      });
+          # Generate a package for each version
+          versionPackages = lib.mapAttrs 
+            (version: _: makeKrakendPackage system version) 
+            (catalog.versions or {});
+            
+          # Only include versions that have a build for this system
+          filteredVersions = lib.filterAttrs (_: value: value != null) versionPackages;
+        in
+          filteredVersions // {
+            default = if builtins.hasAttr latestVersion filteredVersions
+                      then filteredVersions.${latestVersion}
+                      else null;
+          }
+      );
     };
 }
