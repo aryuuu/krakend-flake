d48ae1482c37e38514a61755e2f3672d0ae3b013
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2025-03-14 08:14:18 +0700

    feat: add go version for each version

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
         let
           parts = builtins.split "-" system;
           arch = builtins.elemAt parts 0;
-          platform = builtins.elemAt parts 2;
         in
           if arch == "x86_64" then "amd64"
           else if arch == "aarch64" then "arm64"
@@ -53,31 +52,71 @@
             (build: build.arch == systemArch && build.platform == systemPlatform) 
             null 
             (catalog.versions.${version}.builds or []);
+            
+          # Get Go version associated with this KrakenD version
+          goVersion = catalog.versions.${version}.goVersion or "1.21";
+          
+          # Function to get the appropriate Go package from nixpkgs
+          getGo = goVer:
+            if goVer == "1.19" then pkgs.go_1_19
+            else if goVer == "1.20" then pkgs.go_1_20
+            else if goVer == "1.21" then pkgs.go_1_21
+            else if goVer == "1.22" then pkgs.go_1_22
+            else pkgs.go;  # Default to latest
+            
+          goPkg = getGo goVersion;
         in
           if buildInfo == null then null else
-          pkgs.stdenv.mkDerivation {
-            pname = "krakend";
-            inherit version;
-            
-            src = pkgs.fetchurl {
-              url = buildInfo.url;
-              sha256 = buildInfo.sha256;
+          {
+            package = pkgs.stdenv.mkDerivation {
+              pname = "krakend";
+              inherit version;
+              
+              src = pkgs.fetchurl {
+                url = buildInfo.url;
+                sha256 = buildInfo.sha256;
+              };
+              
+              sourceRoot = ".";
+              
+              nativeBuildInputs = with pkgs; [
+                autoPatchelfHook
+              ];
+              
+              buildInputs = with pkgs; [
+                stdenv.cc.cc.lib
+              ];
+              
+              installPhase = ''
+                mkdir -p $out/bin
+                install -m755 -D usr/bin/krakend $out/bin/krakend
+              '';
             };
             
-            sourceRoot = ".";
+            # Include the compatible Go version
+            go = goPkg;
             
-            nativeBuildInputs = with pkgs; [
-              autoPatchelfHook
-            ];
-            
-            buildInputs = with pkgs; [
-              stdenv.cc.cc.lib
-            ];
-            
-            installPhase = ''
-              mkdir -p $out/bin
-              install -m755 -D usr/bin/krakend $out/bin/krakend
-            '';
+            # Add an overlay that adds a krakend development shell
+            overlay = final: prev: {
+              krakendDevEnv = final.mkShell {
+                packages = [
+                  final.go  # Use the compatible Go version
+                  # final.gopls
+                  # final.delve
+                  # final.golangci-lint
+                ];
+                
+                inputsFrom = [ self.packages.${system}.${version}.package ];
+                
+                shellHook = ''
+                  export KRAKEND_VERSION="${version}"
+                  export GO_VERSION="${goVersion}"
+                  echo "KrakenD Plugin Development Environment"
+                  echo "KrakenD version: $KRAKEND_VERSION"
+                  echo "Go version: $GO_VERSION"
+                '';
+              };
+            };
           };
     in
     {
@@ -93,10 +132,44 @@
             
           # Only include versions that have a build for this system
           filteredVersions = lib.filterAttrs (_: value: value != null) versionPackages;
+          
+          # Map the package attribute to make it the default attribute
+          finalVersions = lib.mapAttrs 
+            (version: value: value.package) 
+            filteredVersions;
         in
-          filteredVersions // {
-            default = if builtins.hasAttr latestVersion filteredVersions
-                      then filteredVersions.${latestVersion}
+          finalVersions // {
+            default = if builtins.hasAttr latestVersion finalVersions
+                      then finalVersions.${latestVersion}
+                      else null;
+          }
+      );
+      
+      # Development shells for each KrakenD version with the correct Go version
+      devShells = eachSystem (system:
+        let
+          shells = lib.mapAttrs 
+            (version: value: 
+              if value == null then null else
+              let 
+                pkgs = pkgsFor.${system}.extend value.overlay;
+              in
+              pkgs.krakendDevEnv
+            ) 
+            (lib.mapAttrs 
+              (version: _: makeKrakendPackage system version) 
+              (catalog.versions or {})
+            );
+          
+          # Only include versions that have a build for this system
+          filteredShells = lib.filterAttrs (_: value: value != null) shells;
+          
+          # Default to latest version
+          latestVersion = lib.head (lib.sort lib.versionAtLeast (builtins.attrNames filteredShells));
+        in
+          filteredShells // {
+            default = if builtins.hasAttr latestVersion filteredShells
+                      then filteredShells.${latestVersion}
                       else null;
           }
       );
